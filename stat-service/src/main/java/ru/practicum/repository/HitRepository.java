d741ed1e53ee555e610adc292bff862585406f54
Ruslan103 <117119513+Ruslan103@users.noreply.github.com>
2023-10-18 22:18:10 +0400

    implemented addHit method

--- stat-service/src/main/java/ru/practicum/repository/HitRepository.java
+++ stat-service/src/main/java/ru/practicum/repository/HitRepository.java
@@ -1,5 +1,7 @@
 package ru.practicum.repository;
 
+
+//import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.practicum.model.Hit;
--- stat-service/src/main/java/ru/practicum/service/HitService.java
+++ stat-service/src/main/java/ru/practicum/service/HitService.java
@@ -1,4 +1,7 @@
 package ru.practicum.service;
 
+import ru.practicum.dto.EndpointHitDto;
+
 public interface HitService {
+    void addHit(EndpointHitDto endpointHitDto);
 }
