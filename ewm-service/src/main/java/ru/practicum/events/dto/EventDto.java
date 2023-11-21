76383a9453d08c392865b7a024de5fa7334ce521
Ruslan103 <117119513+Ruslan103@users.noreply.github.com>
2023-11-21 22:09:53 +0400

    добавил модификаторы доступа

--- ewm-service/src/main/java/ru/practicum/events/dto/EventDto.java
+++ ewm-service/src/main/java/ru/practicum/events/dto/EventDto.java
@@ -18,34 +18,23 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class EventDto {
     private Long id;
-
     private String title;
-
     private String annotation;
     private CategoryDto category;
-
     private Integer confirmedRequests;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdOn;
-
     private String description;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime eventDate;
     private UserShortDto initiator;
-
     private Location location;
-
     private Boolean paid;
-
     private Integer participantLimit;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime publishedOn;
-
     private Boolean requestModeration;
-
     private StateEvent state;
-
     private Long views;
-
 }
 
